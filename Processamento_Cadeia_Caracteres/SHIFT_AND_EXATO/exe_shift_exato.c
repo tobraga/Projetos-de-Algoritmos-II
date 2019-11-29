73c48aab3436bc66a13171025400feddc9847791
Max Braga <maxfilipe.c07@gmail.com>
2019-11-29 08:15:52 -0300

    Dividindo cada algoritmo e ajeitando a função time

---
+++ Processamento_Cadeia_Caracteres/SHIFT_AND_EXATO/exe_shift_exato.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+#include "ShiftAndExato.h"
+
+#define MAXTAMTEXTO     1000
+#define MAXTAMPADRAO    10
+#define MAXCHAR         256
+#define NUMMAXERROS     10
+typedef char TipoTexto[MAXTAMTEXTO];
+typedef char TipoPadrao[MAXTAMPADRAO];
+
+TipoTexto T;
+TipoPadrao P;
+long n, m, k, i;
+
+
+
+
+int main(int argc, char **argv){
+
+    
+  printf(" Tamanho do texto: ");
+  scanf("%ld%*[^\n]", &n); getchar();
+  printf(" Tamanho do padrao: ");
+  scanf("%ld%*[^\n]", &m); getchar();
+  
+  printf(" Texto: ");
+  for (i = 1; i <= n; i++) 
+    { T[i-1] = getchar();
+      if (T[i-1] == '\n')
+      T[i-1] = ' ';
+    }  
+  scanf("%*[^\n]");
+  getchar();
+  printf(" Padrao: ");
+  for (i = 1; i <= m; i++) 
+    { P[i-1] = getchar();
+      if (P[i-1] == '\n')
+      P[i-1] = ' ';
+    }
+    
+  clock_t timer; 
+  timer = clock();
+  
+    
+  printf(" Shift-And-Exato: ");
+  ShiftAndExato(T, n, P, m);
+  scanf("%*[^\n]"); getchar();
+  timer = clock() - timer;
+  printf("\nTempo -> %f segundos\n", ((double)timer)/CLOCKS_PER_SEC);
+  
+  
+}
